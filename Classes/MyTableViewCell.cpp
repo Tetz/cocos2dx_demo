5e94e063f580330782f62290e2bd066f29fe642a
Tetsuro Takemoto <t_takemoto@aeria.jp>
2014-01-16 19:11:13 +0900

    Scene Loop by Swipe

---
+++ Classes/MyTableViewCell.cpp
@@ -0,0 +1,9 @@
+//
+//  MyTableViewCell.cpp
+//  MessagePlus
+//
+//  Created by ted on 1/16/14.
+//
+//
+
+#include "MyTableViewCell.h"
---
+++ Classes/MyTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  MyTableViewCell.h
+//  MessagePlus
+//
+//  Created by ted on 1/16/14.
+//
+//
+
+#ifndef __MessagePlus__MyTableViewCell__
+#define __MessagePlus__MyTableViewCell__
+
+#include <iostream>
+
+#endif /* defined(__MessagePlus__MyTableViewCell__) */
